43f5761eccc5367795976793f198a16e24dfa26b
Mercedes Vera Sotelo <63819921+mverasotelo@users.noreply.github.com>
2021-07-07 12:54:48 -0300

    Add files via upload

---
+++ VeraSoteloTP3-R/Controller.h
@@ -0,0 +1,109 @@
+#ifndef controller_H_INCLUDED
+#define controller_H_INCLUDED
+#include "LinkedList.h"
+#include "Employee.h"
+#include "VeraSotelo.h"
+#include "parser.h"
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_loadFromText(char* path , LinkedList* pArrayListEmployee);
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee);
+
+/** \brief Alta de empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_addEmployee(LinkedList* pArrayListEmployee, int* nextId);
+
+/** \brief Calcula el proximo ID a ser asignado
+ *
+ * \param pArrayListEmployee LinkedList* lista
+ * \param nextId int*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_getId(LinkedList * pArrayListEmployee, int *nextId);
+
+/** \brief Modificar datos de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_editEmployee(LinkedList* pArrayListEmployee);
+
+/** \brief Baja de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_removeEmployee(LinkedList* pArrayListEmployee);
+
+/** \brief Listar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_ListEmployee(LinkedList* pArrayListEmployee);
+
+/** \brief Ordenar empleados por criterios
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_sortEmployee(LinkedList* pArrayListEmployee, int criterio, int orden);
+
+/** \brief Guarda los datos de los empleados en un archivo (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_saveAsText(char* path , LinkedList* pArrayListEmployee);
+
+/** \brief Guarda los datos de los empleados en un archivo (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee);
+
+#endif // controller_H_INCLUDED
+
+
+/** \brief Borra empleados de una lista y borra la lista
+ *
+ * \param pArrayListEmployee LinkedList* lista
+ * \return int - devuelve 1 si salio todo bien, 0 si hubo algun error
+ *
+ */
+int controller_deleteEmployees(LinkedList* pArrayListEmployee);
+
+
---
+++ VeraSoteloTP3-R/Employee.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Employee.h"
+
+Employee* employee_new()
+{
+    Employee* newEmployee = (Employee*) malloc(sizeof(Employee));
+    if (newEmployee != NULL)
+    {
+        newEmployee->id=0;
+        strcpy(newEmployee->nombre,"");
+        newEmployee->horasTrabajadas=0;
+        newEmployee->sueldo=0;
+    }
+    return newEmployee;
+}
+
+Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr,char* sueldo)
+{
+    Employee* newEmployee=employee_new();
+    if (newEmployee != NULL)
+    {
+        if (atoi(idStr) > 0 && atoi(horasTrabajadasStr) > 1)
+        {
+            employee_setId(newEmployee, atoi(idStr));
+            employee_setNombre(newEmployee, nombreStr);
+            employee_setHorasTrabajadas(newEmployee, atoi(horasTrabajadasStr));
+            employee_setSueldo(newEmployee, atoi(sueldo));
+        }
+        else
+        {
+            free(newEmployee);
+            newEmployee = NULL;
+        }
+    }
+    return newEmployee;
+}
+
+void employee_delete(Employee* this)
+{
+    if(this != NULL)
+    {
+        free(this);
+    }
+}
+
+int employee_print(Employee * this){
+    int retorno = 0;
+
+    if(this != NULL){
+        printf(" %4d  %16s      %6d      %10d\n", this->id, this->nombre, this->horasTrabajadas, this->sueldo);
+        retorno = 1;
+    }
+    return retorno;
+}
+
+
+//GETTER Y SETTER ID
+int employee_setId(Employee* this,int id)
+{
+    int retorno=0;
+    if(this != NULL && id > 0)
+    {
+        this->id=id;
+        retorno=1;
+    }
+    return retorno;
+}
+
+
+int employee_getId(Employee* this,int* id)
+{
+    int retorno=0;
+    if( this != NULL && id != NULL)
+    {
+        *id = this->id;
+        retorno=1;
+    }
+    return retorno;
+}
+
+//GETTER Y SETTER NOMBRE
+int employee_setNombre(Employee* this,char* nombre)
+{
+    int retorno=0;
+    if(this != NULL && nombre!=NULL)
+    {
+        strcpy(this->nombre, nombre);
+        retorno=1;
+    }
+    return retorno;
+}
+
+int employee_getNombre(Employee* this,char* nombre)
+{
+    int retorno=0;
+    if( this != NULL && nombre!=NULL )
+    {
+        strcpy(nombre, this->nombre);
+        retorno=1;
+    }
+    return retorno;
+}
+
+//GETTER Y SETTER HORAS TRABAJADAS
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas)
+{
+    int retorno=0;
+    if(this != NULL && horasTrabajadas > 1)
+    {
+        this->horasTrabajadas=horasTrabajadas;
+        retorno=1;
+    }
+    return retorno;
+}
+
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas)
+{
+    int retorno=0;
+    if( this != NULL && horasTrabajadas!=NULL )
+    {
+        *horasTrabajadas = this->horasTrabajadas;
+        retorno=1;
+    }
+    return retorno;
+}
+
+//GETTER Y SETTER SUELDO
+int employee_setSueldo(Employee* this,int sueldo)
+{
+    int retorno=0;
+    if(this != NULL && sueldo > 1)
+    {
+        this->sueldo=sueldo;
+        retorno=1;
+    }
+    return retorno;
+}
+
+int employee_getSueldo(Employee* this,int* sueldo)
+{
+    int retorno=0;
+    if( this != NULL && sueldo != NULL)
+    {
+        *sueldo = this->sueldo;
+        retorno=1;
+    }
+    return retorno;
+}
+
+//comparison
+int employee_compareId(void* a,void* b)
+{
+    int retorno = 0;
+    Employee * emp1 = NULL;
+    Employee * emp2 = NULL;
+
+    if( a != NULL && b != NULL)
+    {
+         emp1 = (Employee*) a;
+         emp2 = (Employee*) b;
+
+        if(emp1->id > emp2 -> id)
+        {
+            retorno = 1;
+        }
+        else if(emp1-> id < emp2 -> id)
+        {
+            retorno = -1;
+        }
+    }
+    return retorno;
+}
+
+
+int employee_compareSueldo(void* a,void* b)
+{
+    int retorno = 0;
+    Employee * emp1 = NULL;
+    Employee * emp2 = NULL;
+
+    if( a != NULL && b != NULL)
+    {
+         emp1 = (Employee*) a;
+         emp2 = (Employee*) b;
+        if(emp1->sueldo > emp2 -> sueldo)
+        {
+            retorno = 1;
+        }
+        else if(emp1-> sueldo < emp2 -> sueldo)
+        {
+            retorno = -1;
+        }
+    }
+    return retorno;
+}
+
+int employee_compareNombre(void* a,void* b)
+{
+    int retorno = 0;
+    Employee * emp1 = NULL;
+    Employee * emp2 = NULL;
+
+    if( a != NULL && b != NULL)
+    {
+         emp1 = (Employee*) a;
+         emp2 = (Employee*) b;
+
+        if(strcmp(emp1-> nombre, emp2-> nombre) == 1)
+        {
+            retorno = 1;
+        }
+        else if(strcmp(emp1-> nombre, emp2-> nombre) == -1)
+        {
+            retorno = -1;
+        }
+    }
+    return retorno;
+}
